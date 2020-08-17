e0b8af64ed5e3f2dd59eea0ee046c9873d9b10d1
John Bateman <john.j.bateman@gmail.com>
2020-08-17 17:22:42 -0400

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -15,7 +15,7 @@ pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' > /etc/pacman.d/mirrorlist
 
-
+pacman -Syyy
 
 echo -e "\nInstalling prereqs...\n$HR"
 pacman -S --noconfirm gptfdisk
