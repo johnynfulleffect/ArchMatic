2a9e19eacd9b4dcfcc81fee3721f72cdd2725afd
John Bateman <john.j.bateman@gmail.com>
2020-08-21 23:33:26 -0400

    Add secure steps

---
+++ 4-secure-system.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#      _          _    __  __      _   _
+#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
+#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
+#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo "-------------------------------------------------"
+echo "Secure Linux                                     "
+echo "-------------------------------------------------"
+
+# --- Setup UFW rules
+sudo ufw limit 22/tcp  
+sudo ufw allow 80/tcp  
+sudo ufw allow 443/tcp  
+sudo ufw default deny incoming  
+sudo ufw default allow outgoing
+sudo ufw enable
+
+# --- Harden /etc/sysctl.conf
+sudo sysctl kernel.modules_disabled=1
+sudo sysctl -a
+sudo sysctl -A
+sudo sysctl mib
+sudo sysctl net.ipv4.conf.all.rp_filter
+sudo sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+
+# --- PREVENT IP SPOOFS
+cat <<EOF > /etc/host.conf
+order bind,hosts
+multi on
+EOF
+
+# --- Enable fail2ban
+sudo cp fail2ban.local /etc/fail2ban/
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
