2c020371cafffca9ecd4b4ebb28d753e1b59e22a
John Bateman <john.j.bateman@gmail.com>
2020-08-21 23:47:17 -0400

    Remove sudo

--- 4-secure-system.sh
+++ 4-secure-system.sh
@@ -12,20 +12,19 @@ echo "Secure Linux                                     "
 echo "-------------------------------------------------"
 
 # --- Setup UFW rules
-sudo ufw limit 22/tcp  
-sudo ufw allow 80/tcp  
-sudo ufw allow 443/tcp  
-sudo ufw default deny incoming  
-sudo ufw default allow outgoing
-sudo ufw enable
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw default deny incoming  
+ufw default allow outgoing
+ufw enable
 
 # --- Harden /etc/sysctl.conf
-sudo sysctl kernel.modules_disabled=1
-sudo sysctl -a
-sudo sysctl -A
-sudo sysctl mib
-sudo sysctl net.ipv4.conf.all.rp_filter
-sudo sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+sysctl kernel.modules_disabled=1
+sysctl -a
+sysctl -A
+sysctl mib
+sysctl net.ipv4.conf.all.rp_filter
+sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
 
 # --- PREVENT IP SPOOFS
 cat <<EOF > /etc/host.conf
@@ -34,6 +33,6 @@ multi on
 EOF
 
 # --- Enable fail2ban
-sudo cp fail2ban.local /etc/fail2ban/
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
+cp fail2ban.local /etc/fail2ban/
+systemctl enable fail2ban
+systemctl start fail2ban
